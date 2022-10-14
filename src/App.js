506be25cba793eb5c37bc134f4eb2909d82fdf45
gerfabris <ggerfabris@gmail.com>
2022-10-14 09:23:51 -0300

    Home, header con background y presentacion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./style.css";
+import "./styles/style.css";
 import { AppRoutes } from "./components/AppRoutes/AppRoutes";
 
 function App() {
